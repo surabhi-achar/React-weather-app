1a73a7a6c443b51d16099e2b853feb52af6fb760
surabhi-achar <surabhiacharya5@gmail.com>
2024-06-27 14:26:06 +0530

    Updates

---
+++ precache-manifest.ef7e0a6f976ef6596c535c5c6eae8748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11a5a4f3b6f0b4accea8063fedcff56",
+    "url": "/React-weather-app/index.html"
+  },
+  {
+    "revision": "ca6b19489b58e8d8d128",
+    "url": "/React-weather-app/static/css/main.d5b55014.chunk.css"
+  },
+  {
+    "revision": "b3c40c76f4e64c2c7d57",
+    "url": "/React-weather-app/static/js/2.732d2c9b.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/React-weather-app/static/js/2.732d2c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6b19489b58e8d8d128",
+    "url": "/React-weather-app/static/js/main.2e228afe.chunk.js"
+  },
+  {
+    "revision": "ea2b4c4a6117fd7ddc62",
+    "url": "/React-weather-app/static/js/runtime-main.b6cdc913.js"
+  }
+]);
